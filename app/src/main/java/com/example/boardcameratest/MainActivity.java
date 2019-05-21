9b45b4e8a8347511c6a11097b689cb28e32b50ce
hollyth2v@gmail.com <!Shdnfkadl88>
2019-05-21 18:02:25 +0900

    first

---
+++ app/src/main/java/com/example/boardcameratest/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.boardcameratest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
